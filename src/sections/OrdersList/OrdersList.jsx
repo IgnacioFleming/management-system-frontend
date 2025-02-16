87f4cf75bf954c11da21e40a61f9645fd194ac06
Ignacio Fleming <ignacioflemings@gmail.com>
2025-02-16 20:04:00 -0300

    delete comments

--- src/sections/OrdersList/OrdersList.jsx
+++ src/sections/OrdersList/OrdersList.jsx
@@ -25,17 +25,7 @@ export default function OrderList({ sale_id }) {
   };
   return (
     <div className="card">
-      {/* <DataTable value={orders} removableSort footer={footer} tableStyle={{ minWidth: "60rem" }}>
-        <Column name="product_name" field="name" header="Producto" sortable></Column>
-        <Column name="product_category" field="category" header="Categoría" sortable></Column>
-        <Column name="quantity" field="quantity" header="Cantidad" body={quantityBodyTemplate} sortable></Column>
-        <Column name="price" field="price" header="Precio" body={priceBodyTemplate} sortable></Column>
-        <Column name="amount" field="amount" header="Total" body={amountBodyTemplate} sortable></Column>
-        <Column header={<ExportButton data={orders} filename="orders.xlsx" />} body={actionsBodyTemplate}></Column>
-      </DataTable> */}
-      <div>
-        <CustomTableContainer columns={columns} items={orders} updating handleUpdate={handleUpdateOrder} deletion handleDelete={deleteOrder} />
-      </div>
+      <CustomTableContainer columns={columns} items={orders} updating handleUpdate={handleUpdateOrder} deletion handleDelete={deleteOrder} />
     </div>
   );
 }
