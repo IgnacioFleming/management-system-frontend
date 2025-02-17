412b7d3ec577799e915c3113d4be72e872ce5dc4
Ignacio Fleming <ignacioflemings@gmail.com>
2025-02-17 20:06:00 -0300

    products service working correctly

--- src/pages/AddProducts/AddProducts.jsx
+++ src/pages/AddProducts/AddProducts.jsx
@@ -17,7 +17,7 @@ function AddProducts() {
     <div className={styles.cardContainer}>
       <div className={`card ${styles.card}`}>
         <div className="flex justify-content-end mb-5">
-          <SelectButton value={selection} onChange={handleSelectChange} options={addModeOptions} />;
+          <SelectButton value={selection} onChange={handleSelectChange} options={addModeOptions} />
         </div>
         <Card pt={{ title: { className: "m-4" } }} title="Alta de Productos" className="flex justify-content-center">
           {selection === addModes.manual ? <AddProductManualForm /> : <ImportButton service={productsService} />}
