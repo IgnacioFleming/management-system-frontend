113565e238bd4afc2fcd09632f7dbf850a4a44b7
Ignacio Fleming <ignacioflemings@gmail.com>
2025-03-17 21:02:00 -0300

    chart render fixed

--- src/pages/Home/SalesChart.jsx
+++ src/pages/Home/SalesChart.jsx
@@ -4,27 +4,22 @@ import { useEffect, useState } from "react";
 function SalesChart({ sales }) {
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
-  const [axis, setAxis] = useState({ x: [], y: [] });
 
   useEffect(() => {
-    if (sales?.length > 0) {
-      const Xaxis = sales.map((sale) => sale.sale_day + "/" + sale.sale_month);
-      const Yaxis = sales.map((sale) => sale.total_amount_per_day.toFixed(2));
-      const axisData = { x: Xaxis, y: Yaxis };
-      setAxis(axisData);
-    }
-  }, [sales]);
-  useEffect(() => {
+    if (sales?.length <= 0) return;
+    const Xaxis = sales.map((sale) => sale.sale_day + "/" + sale.sale_month);
+    const Yaxis = sales.map((sale) => sale.total_amount_per_day.toFixed(2));
+
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue("--text-color");
     const textColorSecondary = documentStyle.getPropertyValue("--text-color-secondary");
     const surfaceBorder = documentStyle.getPropertyValue("--surface-border");
     const data = {
-      labels: axis.x,
+      labels: Xaxis,
       datasets: [
         {
           label: "Ventas Diarias",
-          data: axis.y,
+          data: Yaxis,
           fill: false,
           borderColor: documentStyle.getPropertyValue("--cyan-500"),
           tension: 0.4,
@@ -62,13 +57,13 @@ function SalesChart({ sales }) {
     };
     setChartData(data);
     setChartOptions(options);
-  }, [axis]);
+  }, [sales]);
 
   return (
     <div className="card w-full flex flex-column align-items-center justify-content-center mt-8">
       <div className="sm:w-full md:w-7">
         <h3 className="text-center">Ventas del último Mes</h3>
-        <Chart type="line" data={chartData} options={chartOptions} className="w-12 h-20rem md:h-full md:h-12" />
+        {chartData?.labels?.length > 0 && chartData?.datasets?.length > 0 && <Chart type="line" data={chartData} options={chartOptions} className="w-12 h-20rem md:h-full md:h-12" />}
       </div>
     </div>
   );
