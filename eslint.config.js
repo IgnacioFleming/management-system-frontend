9271ef1a111b52e2aeed0340b3f393b5dbf273b6
Ignacio Fleming <ignacioflemings@gmail.com>
2025-03-18 21:04:00 -0300

    set up jest for testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 import js from "@eslint/js";
-import globals from "globals";
+import globals, { jest } from "globals";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
@@ -32,6 +32,9 @@ export default [
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "react/prop-types": "off",
       "react-hooks/exhaustive-deps": "off",
+    },
+    env: {
+      jest: true,
     },
   },
 ];
