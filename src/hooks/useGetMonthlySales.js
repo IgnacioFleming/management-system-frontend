99ca5291911afa6197361ea5533529f0dcf97721
Ignacio Fleming <ignacioflemings@gmail.com>
2025-03-23 21:14:00 -0300

    avoid getMonthlySales call when server is asleep

--- src/hooks/useGetMonthlySales.js
+++ src/hooks/useGetMonthlySales.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { salesService } from "../services";
+import { UserContext } from "../contexts/user";
 
 export const useGetMonthlySales = (initial) => {
+  const { user } = useContext(UserContext);
   const [data, setData] = useState(initial);
   useEffect(() => {
+    if (Object.keys(user).length <= 0) return;
     getMonthlySales();
   }, []);
   const getMonthlySales = async () => {
